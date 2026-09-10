c9ccdae4d970cc51648712253812a5fbe8363b3e
Shivendra Devadhe <devadhe.shivendra+github@protonmail.com>
2026-09-10 19:47:37 +0530

    chore: initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS Student Builder Group Website
+
+- This repository hosts source code for official website of AWS SBG club in AISSMS IOIT.
---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "IOIT AWS SBG",
+  description: "Official club website.",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html
+      lang="en"
+      className={`h-full antialiased`}
+    >
+      <body
+        className="min-h-full flex flex-col"
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
